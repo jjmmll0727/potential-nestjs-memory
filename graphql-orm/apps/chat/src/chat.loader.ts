6904295cfa10fc757f1def813e54c1310bf01503
jjmmll0727 <jjmmll0727@gmail.com>
2023-12-04 11:11:42 +0900

    custom dataloader

--- graphql-orm/apps/chat/src/chat.loader.ts
+++ graphql-orm/apps/chat/src/chat.loader.ts
@@ -4,7 +4,6 @@ import { UserModel } from './entities/user.model';
 
 /**
  * @description resolveReference 에서 디비에서 찾는데, dataloader 를 사용하면 여기서 찾을 수 있다.
- * { '1': [ { id: '1' } ], '2': [ { id: '2' } ] } 반드시 이 형식으로 보내야 한다.
  */
 @Injectable({ scope: Scope.REQUEST })
 export class ChatLoader {
@@ -13,23 +12,35 @@ export class ChatLoader {
       console.log(roomIds);
       //   const orderItems: UserModel[] =
       //     await this.orderItemRepo.findByOrderIds(orderIds);
-      const userModels: UserModel[] = [
-        { id: '1' },
-        { id: '2' },
-        { id: '3' },
-        { id: '4' },
-        { id: '5' },
-        { id: '6' },
-      ];
+      // const userModels: UserModel[] = [
+      //   { id: '1' },
+      //   { id: '2' },
+      //   { id: '3' },
+      //   { id: '4' },
+      //   { id: '5' },
+      //   { id: '6' },
+      // ];
+
+      const userModels2 = {
+        4: [{ id: '4' }, { id: '8' }, { id: '12' }],
+        5: [{ id: '5' }, { id: '10' }, { id: '15' }],
+        6: [{ id: '6' }, { id: '12' }, { id: '18' }],
+      };
       const userModelGroup: { [key: number]: UserModel[] } = {};
-      userModels.forEach((userModel: UserModel) => {
-        if (!userModelGroup[userModel.id]) {
-          userModelGroup[userModel.id] = [];
+      for (const roomId of roomIds) {
+        if (!userModelGroup[roomId]) {
+          userModelGroup[roomId] = [];
         }
-        userModelGroup[userModel.id].push(userModel);
-      });
-      console.log('userModelGroup ->', userModelGroup);
-      console.log(roomIds.map((roomId: number) => userModelGroup[roomId]));
+        userModelGroup[roomId] = userModels2[roomId];
+      }
+      // userModels.forEach((userModel: UserModel) => {
+      //   if (!userModelGroup[userModel.id]) {
+      //     userModelGroup[userModel.id] = [];
+      //   }
+      //   userModelGroup[userModel.id].push(userModel);
+      // });
+      console.log(userModelGroup);
+      // console.log(roomIds.map((roomId: number) => userModelGroup[roomId]));
       return roomIds.map((roomId: number) => userModelGroup[roomId]);
     },
   );
